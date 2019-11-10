88d2fa40f6e1aeedc965c8dc82731d78a0b03d7c
rlefrancoise <renaud.lefrancoise@gmail.com>
2019-11-10 18:09:43 +0100

    Fix cmakelists + add clion folders to git ignore + open default window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "Black Fox");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
     return 0;
 }
